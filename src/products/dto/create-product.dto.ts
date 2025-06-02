2fec4d8a7345af28d801dcf551949cbbfb9928aa
Ivan Khuda <ivanchuda@gmail.com>
2025-06-02 18:56:09 +0200

    feat: products create dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { ProductStatus } from '../entities/product.entity';
+
+export class CreateProductDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  body: string;
+
+  @IsString()
+  @IsOptional()
+  tags: string;
+
+  @IsEnum(ProductStatus)
+  @IsOptional()
+  status: ProductStatus = ProductStatus.DRAFT;
+}
